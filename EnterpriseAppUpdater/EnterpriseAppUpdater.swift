06e56027ce5cecb78209331e6c5c145280fddf69
Alexey Demin <dnv1ex@yahoo.com>
2019-09-09 09:45:23 -0300

    Initial Commit

---
+++ EnterpriseAppUpdater/EnterpriseAppUpdater.swift
@@ -0,0 +1,11 @@
+//
+//  EnterpriseAppUpdater.swift
+//  EnterpriseAppUpdater
+//
+//  Created by Alexey Demin on 2019-09-09.
+//  Copyright © 2019 DnV1eX. All rights reserved.
+//
+
+class EnterpriseAppUpdater {
+
+}
